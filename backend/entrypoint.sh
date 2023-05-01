85d1a5bd44149683eee222cdf5bc290d2235204c
Tim Arnold <frenggie@gmx.de>
2023-05-02 01:27:42 +0200

    Add: dockerization finally done i hope

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Flush the manage.py command it any"
+#echo "Flush the manage.py command it any"
 
-while ! python manage.py flush --no-input 2>&1; do  # Obviously needs to be removed for production lol
- echo "Flusing django manage command"
- sleep 3
-done
+#while ! python manage.py flush --no-input 2>&1; do  # Obviously needs to be removed for production lol
+# echo "Flusing django manage command"
+# sleep 3
+#done
 
 echo "Migrate the Database at startup of project"
 
--- backend/setup.py
+++ backend/setup.py
@@ -7,4 +7,5 @@ user, created = get_user_model().objects.get_or_create(is_superuser=True, is_sta
 # Genres
 thriller, created = Genre.objects.get_or_create(name="Thriller", description="Aufregende Geschichten über die Gefahren und Geheimnisse unseres Nächsten.")
 crimi, created = Genre.objects.get_or_create(name="Krimi", description="Let's get the party going!")
-horror, created = Genre.objects.get_or_create(name="Horror", description="Grußelige Geschichten, die einem das Blut in den Adern gefrieren lassen")
+horror, created = Genre.objects.get_or_create(name="Horror", description="Grußelige Geschichten, die einem das Blut in den Adern gefrieren lassen.")
+science_fiction, created = Genre.objects.get_or_create(name="Science Fiction", description="Futuristische Stories über zukünftig zu tragende Laster, die Unendlichkeit und das Unbekannte.")
